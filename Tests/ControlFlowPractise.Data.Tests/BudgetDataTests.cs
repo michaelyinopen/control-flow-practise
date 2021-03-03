f026d7c9b7d8cc45b0ea554c88a23d3d4b5c6260
michaelyinopen <michaelyinopen@gmail.com>
2021-03-03 16:43:56 +1100

    Tested budget data

--- Tests/ControlFlowPractise.Data.Tests/BudgetDataTests.cs
+++ Tests/ControlFlowPractise.Data.Tests/BudgetDataTests.cs
@@ -1,5 +1,9 @@
 using ControlFlowPractise.BudgetData;
+using ControlFlowPractise.BudgetData.Models;
+using ControlFlowPractise.Common;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -20,17 +24,104 @@ namespace ControlFlowPractise.Data.Tests
             using var context = new BudgetDataDbContext(DbContextOptions);
         }
 
-        // Can add
-        // Can read after add
-        // - count
-        // - has date as utc
-        // - operation works
-        // x request x response
         [Fact]
-        public async Task CanReadExternalPartyRequestCount()
+        public async Task CanAddExternalPartyRequest()
         {
             using var context = new BudgetDataDbContext(DbContextOptions);
-            var result = await context.ExternalPartyRequest.CountAsync();
+            context.ExternalPartyRequest.Add(new ExternalPartyRequest(
+                orderId: "1",
+                request: "{}")
+            {
+                Operation = WarrantyCaseOperation.Create,
+                RequestId = Guid.NewGuid()
+            });
+            await context.SaveChangesAsync();
+        }
+
+        [Fact]
+        public async Task CanCountExternalPartyRequest()
+        {
+            using var context = new BudgetDataDbContext(DbContextOptions);
+            var count = await context.ExternalPartyRequest.CountAsync();
+        }
+
+        [Fact]
+        public async Task CanReadExternalPartyRequestByOrderIdAndRequestId()
+        {
+            string orderId = "2";
+            Guid requestId = Guid.NewGuid();
+            using (var addingContext = new BudgetDataDbContext(DbContextOptions))
+            {
+                addingContext.ExternalPartyRequest.Add(new ExternalPartyRequest(
+                    orderId,
+                    request: "{}")
+                {
+                    Operation = WarrantyCaseOperation.Verify,
+                    RequestId = requestId
+                });
+                await addingContext.SaveChangesAsync();
+            }
+            using (var readingContext = new BudgetDataDbContext(DbContextOptions))
+            {
+                var actual = await readingContext.ExternalPartyRequest
+                    .Where(req => req.OrderId == orderId)
+                    .Where(req => req.RequestId == requestId)
+                    .FirstOrDefaultAsync();
+                Assert.Equal(orderId, actual.OrderId);
+                Assert.Equal(requestId, actual.RequestId);
+                Assert.Equal(WarrantyCaseOperation.Verify, actual.Operation);
+                Assert.Equal(DateTimeKind.Utc, actual.DateTime.Kind);
+            }
+        }
+
+        [Fact]
+        public async Task CanAddExternalPartyResponse()
+        {
+            using var context = new BudgetDataDbContext(DbContextOptions);
+            context.ExternalPartyResponse.Add(new ExternalPartyResponse(
+                orderId: "1",
+                response: "{}")
+            {
+                Operation = WarrantyCaseOperation.Create,
+                RequestId = Guid.NewGuid()
+            });
+            await context.SaveChangesAsync();
+        }
+
+        [Fact]
+        public async Task CanCountExternalPartyResponse()
+        {
+            using var context = new BudgetDataDbContext(DbContextOptions);
+            var count = await context.ExternalPartyResponse.CountAsync();
+        }
+
+        [Fact]
+        public async Task CanReadAddedExternalPartyResponse()
+        {
+            string orderId = "2";
+            Guid requestId = Guid.NewGuid();
+            using (var addingContext = new BudgetDataDbContext(DbContextOptions))
+            {
+                addingContext.ExternalPartyResponse.Add(new ExternalPartyResponse(
+                    orderId,
+                    response: "{}")
+                {
+                    Operation = WarrantyCaseOperation.Verify,
+                    RequestId = requestId
+                });
+                await addingContext.SaveChangesAsync();
+            }
+            using (var readingContext = new BudgetDataDbContext(DbContextOptions))
+            {
+                var actual = await readingContext.ExternalPartyResponse
+                    .Where(req => req.OrderId == orderId)
+                    .Where(req => req.RequestId == requestId)
+                    .FirstOrDefaultAsync();
+                Assert.Equal(orderId, actual.OrderId);
+                Assert.Equal(requestId, actual.RequestId);
+                Assert.Equal(WarrantyCaseOperation.Verify, actual.Operation);
+                Assert.Equal(DateTimeKind.Utc, actual.DateTime.Kind);
+            }
         }
     }
 }
